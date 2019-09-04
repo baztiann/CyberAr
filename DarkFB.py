29349e0e9c31626dbb80603e23bc3fd6940ecdfa
baztiann <54882236+baztiann@users.noreply.github.com>
2019-09-04 21:31:48 +0700

    Update and rename FB.py to DarkFB.py

--- DarkFB.py
+++ DarkFB.py
@@ -1,24 +1,3 @@
-#!/bin/bash
-#version 1.0
-
-# Variables
-b='\033[1m'
-u='\033[4m'
-bl='\E[30m'
-r='\E[31m'
-g='\E[32m'
-bu='\E[34m'
-m='\E[35m'
-c='\E[36m'
-w='\E[37m'
-endc='\E[0m'
-enda='\033[0m'
-blue='\e[1;34m'
-cyan='\e[1;36m'
-red='\e[1;31m'
-
-figlet baztian | lolcat
-
 echo -e  "_____________________________________________________________"
 echo -e  "Tools    : 0daysecurity98 $white         " |lolcat
 echo -e  "Creadby  : 0day $white   " |lolcat
@@ -32,7 +11,6 @@ def tik():
         print '\r\x1b[1;91m[\xe2\x97\x8f] \x1b[1;92mSedang Proses Masuk \x1b[1;97m' + o,
         sys.stdout.flush()
         time.sleep(1)
-
 
 back = 0
 threads = []
